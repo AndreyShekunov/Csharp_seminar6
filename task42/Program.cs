3054e45e7034e27834dae79df90143c84b183ee6
Шекунов Андрей <asheknov@gmail.com>
2022-09-18 12:55:38 +0500

    Задача 42 преобразовать десятичное число в двоичное с помощью рекурсии

---
+++ task42/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// Например:
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+void PintBinaryView(int number) // метод вывода на экран двоичного числа
+{
+    if (number <= 0)
+    {
+        return;
+    }
+    else
+    {
+        PintBinaryView(number / 2);
+        Console.Write(number % 2);
+    }
+}
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+PintBinaryView(num);
